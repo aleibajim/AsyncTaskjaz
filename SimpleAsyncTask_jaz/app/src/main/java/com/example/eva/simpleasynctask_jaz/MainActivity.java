483ffb01024dae978fb9a68215f6294eccb730a9
jazmin12 <jazmin_salazar@outlook.com>
2017-05-02 20:52:19 -0600

    xdfghjk
    dfgjk

---
+++ SimpleAsyncTask_jaz/app/src/main/java/com/example/eva/simpleasynctask_jaz/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.eva.simpleasynctask_jaz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void startTask (View view){
+        mTextView.setText("Napping");
+
+        //inicializa el AsyncTask
+        new SimpleAsyncTask(mTextView).execute();
+    }
+
+    private TextView mTextView;
+}
